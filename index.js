27d90602b8f1423cb645a46f003511c928d66075
Minh,Quan <kyrin232002@gmail.com>
2022-01-23 05:42:54 +0700

    last commit

--- index.js
+++ index.js
@@ -3,99 +3,93 @@ let address ="Hoàng Triệu Minh Quân, BC19-Javascript3"
 console.log(address);
 // Bai 1
 /* *Lương 1 ngày : 100.000
-Bước 1 : tạo biến cho lương 1 ngày
-Bước 2 : tạo biến và gán giá trị cho số ngày đi làm
+Bước 1 : tạo input và button để người dùng nhập và xuất kết quả
+Bước 2 : Cho người dùng nhập vào số ngày làm việc 
 Bước 3 : Sử dụng công thức Lương nhân viên =(số ngày đi làm * lương 1 ngày)
-Bước 4 : in lương của nhân viên ra console*/
+Bước 4 : in lương của nhân viên ra console và html*/
 
-const Bai_1 ="Bai 1"
-console.log(Bai_1);
-
-const x = document.getElementById("Address".value);
-const AddressEl = document.getElementById("Address");
-const resultEl = document.getElementById("result");
-resultEl.onclick = function() {
-    mul = x * 100000;
-    alert(mul);
+document.getElementById("pay").onclick = function () {
+    let dayEl = document.getElementById("day").value;
+    let pay = dayEl * 100000;
+    console.log("Tien luong nhan duoc la:",pay);
+    document.getElementById("bai1").innerHTML = `Tien luong cua ban la: ${pay}`
 }
+
+
 
 
 
 
 // Bai 2
 /*
-Bước 1 : tạo biến a,b,c,d,e cho 5 số thực,sum,div
-Bước 2 : gán giá trị cho 5 số thực 
+Bước 1 : tạo biến cho 5 số thực
+Bước 2 : Cho người dùng nhập vào 5 số thực
 Bước 3 : Sử dụng công thức Trung bình = tổng của 5 số / 5
-Bước 4 : in kết quả trung bình ra console*/
-const Bai_2 ="Bai 2"
-console.log(Bai_2);
+Bước 4 : in kết quả trung bình ra console và html*/
 
-const a= 2;
-const b= 3;
-const c= 4;
-const d= 5;
-const e= 6;
-console.log("Nhap vao 5 so thuc:",a,b,c,d,e);
 
-const sum = a + b + c + d + e;
-const div = sum / 5;
-console.log("Gia tri trung binh la:", div);
+document.getElementById("total").onclick = function calc() {
+    let num1 = Number(document.querySelector("#num1").value);
+    let num2 = Number(document.querySelector("#num2").value);let num3 = Number(document.querySelector("#num3").value);let num4 = Number(document.querySelector("#num4").value);let num5 = Number(document.querySelector("#num5").value);
+    let total = (num1 + num2 + num3 + num4 + num5) / 5;
+    console.log("Trung binh cong la:",total);
+    document.getElementById("bai2").innerHTML = `Trung binh cong la: ${total}` 
+}
+
+
 
 //Bai 3
 /* 1 USD = 23.500 VND
-Bước 1 : tạo biến X,Y, multiply
-Bước 2 : gán giá trị cho Y
+Bước 1 : tạo biến 
+Bước 2 : Cho người dùng nhập vào số tiền muốn đổi
 Bước 3 : Sử dụng công thức số tiến USD * giá VNĐ hiện nay = số tiến VNĐ
-Bước 4 : in số tiền sau khi quy đổi sang VNĐ ra console*/
-const Bai_3 ="Bai 3"
-console.log(Bai_3);
+Bước 4 : in số tiền sau khi quy đổi sang VNĐ ra console và html*/
 
-const X = 23500;
-console.log("Gia VNĐ hien nay:", X, "VND");
-const Y = 50;
-console.log("Nhap vao so tien USD:", Y, "USD");
-const multiply = X * Y;
-console.log("So tien sau khi quy doi sang VND", multiply, "VND");
+document.getElementById("exchange").onclick = function () {
+    let moneyEl = document.getElementById("money").value;
+    let exchange = moneyEl * 23500;
+    console.log("So tien sau khi quy doi la:",exchange);
+    document.getElementById("bai3").innerHTML = `So tien sau khi quy doi la: ${exchange}`
+}
 
 //Bai 4
 /*
-Bước 1 : tạo biến 2 biến là chiều dài và chiều rộng HCN
-Bước 2 : gán giá trị cho 2 biến 
+Bước 1 : tạo 2 biến là chiều dài và chiều rộng HCN
+Bước 2 : Cho người dùng nhập vào CD và CR 
 Bước 3 : Sử dụng công thức :
 Diện tích = Dài * Rộng
 Chu vi = (Dài + Rộng)*2
-Bước 4 : in kết quả diện tích và chu vi ra console*/
-const Bai_4 ="Bai 4"
-console.log(Bai_4);
+Bước 4 : in kết quả diện tích và chu vi ra console và html*/
 
-const CD = 5;
-console.log("Chieu dai HCN:", CD);
-const CR = 4;
-console.log("Chieu rong HCN:", CR);
+document.getElementById("result").onclick = function () {
+    let CD = Number(document.querySelector("#CD").value);
+    let CR = Number(document.querySelector("#CR").value);
+    let dientich = CD * CR;
+    let chuvi = (CD + CR) / 2;
+    console.log("Dien tich hinh chu nhat:",dientich);
+    console.log("Chu vi hinh chu nhat:",chuvi);
+    document.getElementById("bai4").innerHTML = `Diện tích hình chữ nhật là: ${dientich}`
+    document.getElementById("bai4.").innerHTML = `Chu vi hinh chu nhat la: ${chuvi}`
 
-const DT = CD * CR;
-console.log("Dien tich HCN:", DT);
-const CV = (CD + CR)* 2;
-console.log("Chu vi HCN:", CV);
+}
 
 //Bai 5
 /*
-Bước 1 : tạo biến n, ten, unit,Tong
-Bước 2 : gán giá trị cho n
+Bước 1 : tạo biến ten, unit,Tong,number
+Bước 2 : Cho người dùng nhập vào số 
 Bước 3 : tách số hàng chục theo công thức 
 ten = Math.floor(n / 10);
 Bước 4 : tách số hàng đơn vị theo công thức 
 unit = Math.floor(n % 10);
-Bước 5 : in kết quả Tong ra console*/
-const Bai_5 ="Bai 5"
-console.log(Bai_5);
+Bước 5 : in kết quả Tong ra console và html*/
 
-let ten,unit;
-const n= 84;
-console.log("Nhap vao so co 2 chu so la:",n);
-
-ten = Math.floor(n / 10);
-unit = Math.floor(n % 10);
-const Tong = ten + unit;
-console.log("Tong cua 2 ky so la:", Tong);
+document.getElementById("Final").onclick = function () {
+    let number = Number(document.querySelector("#number").value);
+    let ten = Math.floor(number / 10);
+    let unit = Math.floor(number % 10 );
+    let Tong = ten + unit;
+    console.log("Số hàng chục là:",ten);
+    console.log("Số hàng đơn vị là:",unit);
+    console.log("Tổng của 2 kỳ số là:",Tong);
+    document.getElementById("bai5").innerHTML = `Tổng của 2 ký số là: ${Tong}`
+}
