f38c4d5b2996fa4213ca6557a57741047751d951
Minh,Quan <kyrin232002@gmail.com>
2022-02-18 18:57:44 +0700

    second commit

--- index.js
+++ index.js
@@ -1,36 +1,77 @@
 let address ="Hoàng Triệu Minh Quân, BC19-Javascript5"
 
 console.log(address);
+// Bai 1
+const area_A = 2;
+const area_B = 1;
+const area_C = 0.5;
+const area_D = 0;
+const type_1 = 2.5;
+const type_2 = 1.5;
+const type_3 = 1;
+const type_4 = 0;
 
 document.getElementById("result").onclick = function calc() {
-    let day = Number(document.querySelector("#day").value);
-    let num1 = Number(document.querySelector("#num1").value);
-    let num2 = Number(document.querySelector("#num2").value);let num3 = Number(document.querySelector("#num3").value);
-    const total = num1 + num2 + num3;
-    const areaEL = document.getElementById("area").value;
-    if(areaEL === "A"){
-        total + 2;
-        return;
-    }else if(total === "B"){
-        total + 1;
-    }else if(total === "C"){
-        total + 0.5;
-    }
-    const typeEL = document.getElementById("type").value;
-    if(typeEL === "1"){
-        total + 2.5;
-    }else if(typeEL === "2"){
-        total + 1.5;
-    }else if(typeEL === "3"){
-        total + 1;
-    }
+    const standardPoint = document.getElementById("standard").value;
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+    const num3 = parseFloat(document.getElementById("num3").value);
     
-    if (total >= day) {
-        return result ="đậu";
-    }else if (total < day) {
-        return result="rớt";
-    }
+    const areaA = document.getElementById("areaA");
+    const areaB = document.getElementById("areaB");
+    const areaC = document.getElementById("areaC");
+    const areaD = document.getElementById("areaD");
 
-    console.log("Tổng điểm thi:",result);
-    document.getElementById("bai1").innerHTML = `Thí sinh : ${result}` 
+    const type1 = document.getElementById("type1");
+    const type2 = document.getElementById("type2");
+    const type3 = document.getElementById("type3");
+    const type4 = document.getElementById("type4");
+
+    const priorityArea = checkPriorityArea(areaA, areaB, areaC, areaD);
+    const prioritySubject = checkPrioritySubject(type1, type2, type3, type4);
+
+    const finalPoint = calcFinalPoint(num1, num2, num3, priorityArea, prioritySubject);
+
+    if(finalPoint >= standardPoint){
+        document.getElementById("result").innerHTML = 
+          "Thí sinh đậu với " + finalPoint + " điểm.";
+    }else{
+        document.getElementById("result").innerHTML = 
+          "Thí sinh rớt với " + finalPoint + " điểm.";
+}
+}
+
+function calcFinalPoint(num1, num2, num3, priorityArea, prioritySubject) {
+    let finalPoint = 0;
+
+    finalPoint = num1 + num2 + num3 + priorityArea + prioritySubject;
+
+    return finalPoint;
+}
+
+function checkPriorityArea(areaA, areaB, areaC, areaD){
+    if(areaA.checked){
+        return area_A;
+    }else if(areaB.checked){
+        return area_B;
+    }else if (areaC.checked){
+        return area_C;
+    }else if (areaD.checked){
+        return area_D;
+    }else{
+        return 0;
+    }
+}
+function checkPrioritySubject(type1, type2, type3, type4){
+    if(type1.checked){
+        return type_1;
+    }else if(type2.checked){
+        return type_2;
+    }else if (type3.checked){
+        return type_3;
+    }else if(type4.checked){
+        return type_4;
+    }else{
+        return 0;
+    }
 }
